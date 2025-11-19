6e0845ae6b6d1a825e24bc884affbf3cb753ad58
adarshtmg2060 <adarshtmg2060@gmail.com>
2025-11-19 19:42:41 +0545

    fixed

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -4,6 +4,6 @@ export default axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:8080/api/v1"
-      : "https://mero-geet.vercel.app/api/v1",
+      : "https://mero-geet.onrender.com/api/v1",
   withCredentials: true,
 });
